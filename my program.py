84a39b57b6228012689d293ad8ca005ea57b4160
unknown <bandiaruna@kluniversity.in>
2022-01-17 07:58:11 +0530

    My Initial commit

---
+++ my program.py
@@ -0,0 +1,1 @@
+print("shaik mastan saheb")
---
+++ skbb.py
@@ -0,0 +1,16 @@
+"""for number in "2000030923":
+    print("current number = ",number)"""
+
+'''marks=[20,25,15,10,50,100]
+print(type(marks))
+for i in range(len(marks)):
+    print("list iteam",marks[i])'''
+
+'''person = [123,456,"java","tn","ap",4500.78,"kl"]
+print(type(person))
+for i in range(len(person)):
+    print("current loop number : ",person[i])
+'''
+
+for num in range(10,20):
+    print(num)
