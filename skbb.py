484a58de400027fbae0269f962a5cc6fc8a70e33
brahmender <2000030923@kluniversity.in>
2022-01-17 08:06:48 +0530

    I had done the change

--- skbb.py
+++ skbb.py
@@ -1,10 +1,10 @@
 """for number in "2000030923":
     print("current number = ",number)"""
 
-'''marks=[20,25,15,10,50,100]
+marks=[20,25,15,10,50,100]
 print(type(marks))
 for i in range(len(marks)):
-    print("list iteam",marks[i])'''
+    print("list iteam",marks[i])
 
 """person = [123,456,"java","tn","ap",4500.78,"kl"]
 print(type(person))
